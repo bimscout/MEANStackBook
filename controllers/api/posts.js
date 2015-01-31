87e9cb490db570a40d44bbc4abffb72b55419782
bimscout <github@bimscout.com>
2015-01-31 08:38:55 -0800

    Added boot.js, pubsub with reds

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,28 +1,29 @@
-var Post = require('../../models/post')
 var router = require('express').Router()
 var websockets = require('../../websockets')
+var pubsub = require('../../pubsub')
+var Post = require('../../models/post')
 
-router.get('/', function (req, res, next) {
-
+router.get('/posts', function (req, res, next) {
     Post.find()
         .sort('-date')
-        .exec(function(err, posts) {
-            if (err) {return next(err)}
-            res.status(200).json(posts)
+        .exec(function (err, posts) {
+            if (err) { return next(err) }
+            res.json(posts)
         })
-
 })
 
-router.post('/', function (req, res, next) {
-
-    var post = new Post({ body: req.body.body })
+router.post('/posts', function (req, res, next) {
+    var post = new Post({body: req.body.body})
     post.username = req.auth.username
     post.save(function (err, post) {
         if (err) { return next(err) }
-        websockets.broadcast('new_post', post)
+        pubsub.publish('new_post', post)
         res.status(201).json(post)
     })
+})
 
+pubsub.subscribe('new_post', function (post) {
+    websockets.broadcast('new_post', post)
 })
 
 module.exports = router
